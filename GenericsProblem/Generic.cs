57e752916f8b76cb3414daca36a84bc45fcdab19
nk321626 <nk321626@gmail.com>
2022-05-13 23:28:52 +0530

    Added Given Three Integer Find the Maximum

---
+++ GenericsProblem/Generic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProblem
+{
+    public class Generic
+    {
+        public static int FindMaxValueOutOfThree(int first, int second, int third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0) 
+            {
+                return first;
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                return third;
+            }
+            else
+            {
+                Console.WriteLine("\teither 2 or 3 having same value/equals");
+                return 0;
+            }
+        }
+    }
+}
--- GenericsProblem/Program.cs
+++ GenericsProblem/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("=========Welcome to Generic Problems==========");
+﻿using GenericsProblem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace GenericProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========Welcome to Generic Problem==========");
+            Console.WriteLine("\t->Please Select the Option Which can be Executed<-");
+            Console.WriteLine("\t1. FindMaxValueOutOfThree \n\t2. Exit");
+            Console.Write("\tEnter The Selected Option: ");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    int x = 40, y = 30, z = 10;
+                    if (Generic.FindMaxValueOutOfThree(x, y, z) != 0)
+                        Console.WriteLine("\t{0} is maximum out of {1},{2},{3}", Generic.FindMaxValueOutOfThree(x, y, z), x, y, z);
+                    break;
+
+                    case 2:
+                default:
+                    break;
+            }
+            Console.WriteLine("=============================================================");
+            Console.ReadLine();
+        }
+    }
+}
